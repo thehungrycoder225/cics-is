23307716a3f5a760d0539d1af786cdd432cb5bb8
Dane <cyber_sky09@yahoo.com>
2023-02-15 11:20:27 +0800

    modified student trend chart

--- pages/student.js
+++ pages/student.js
@@ -1,52 +1,111 @@
-const trendChart = () => {
-  const xLabel = [
-    ' 2015-2016',
-    ' 2016-2017',
-    ' 2017-2018',
-    ' 2018-2019',
-    ' 2019-2020',
-    ' 2020-2021',
-    ' 2021-2022',
-    ' 2022-2023',
-  ];
+// const trendChart = () => {
+//   const xLabel = [
+//     ' 2015-2016',
+//     ' 2016-2017',
+//     ' 2017-2018',
+//     ' 2018-2019',
+//     ' 2019-2020',
+//     ' 2020-2021',
+//     ' 2021-2022',
+//     ' 2022-2023',
+//   ];
 
-  new Chart('trendChart', {
-    type: 'line',
-    data: {
-      labels: xLabel,
-      datasets: [
+//   new Chart('trendChart', {
+//     type: 'line',
+//     data: {
+//       labels: xLabel,
+//       datasets: [
+//         {
+//           data: [1279, 1211, 1067, 1073, 1093, 1168, 1346, 1535],
+//           label: 'BS Information Technology',
+//           borderColor: '#1BA605',
+//           backgroundColor: '#1BA605',
+//         },
+//         {
+//           data: [490, 351, 232, 226, 246, 270, 275, 350],
+//           label: 'BS Information Systems',
+//           borderColor: '#FBAA30',
+//           backgroundColor: '#FBAA30',
+//         },
+//         {
+//           data: [1769, 1562, 1299, 1299, 1339, 1438, 1621, 1885],
+//           label: 'Total',
+//           borderColor: '#141414',
+//           backgroundColor: '#141414',
+//         },
+//       ],
+//     },
+//     options: {
+//       legend: { position: 'bottom' },
+//       scales: {
+//         y: {
+//           beginAtZero: true,
+//         },
+//       },
+//     },
+//   });
+// };
+
+// trendChart();
+
+const chartLabel = [
+  ' 2015-2016',
+  ' 2016-2017',
+  ' 2017-2018',
+  ' 2018-2019',
+  ' 2019-2020',
+  ' 2020-2021',
+  ' 2021-2022',
+  ' 2022-2023',
+];
+var ctx = document.getElementById('trendChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: chartLabel,
+    datasets: [
+      {
+        label: 'Information Technology',
+        data: [1279, 1211, 1067, 1073, 1093, 1168, 1346, 1535],
+        backgroundColor: 'rgba(26, 166, 5, 0.5)',
+        borderColor: 'rgba(26, 166, 5, 1)',
+        borderWidth: 1,
+      },
+      {
+        label: 'Information Systems',
+        data: [490, 351, 232, 226, 246, 270, 275, 350],
+        backgroundColor: 'rgba(251, 170, 48, 0.5)',
+        borderColor: 'rgba(251, 170, 48, 1)',
+        borderWidth: 1,
+      },
+      {
+        label: 'Total',
+        data: [1769, 1562, 1299, 1299, 1339, 1438, 1621, 1885],
+        type: 'line',
+        backgroundColor: 'rgba(91, 0, 0, 0.5)',
+        borderColor: 'rgba(91, 0, 0, 1)',
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      x: [
         {
-          data: [1279, 1211, 1067, 1073, 1093, 1168, 1346, 1535],
-          label: 'BS Information Technology',
-          borderColor: '#1BA605',
-          backgroundColor: '#1BA605',
+          stacked: true,
+          beginAtZero: true,
         },
+      ],
+      y: [
         {
-          data: [490, 351, 232, 226, 246, 270, 275, 350],
-          label: 'BS Information Systems',
-          borderColor: '#FBAA30',
-          backgroundColor: '#FBAA30',
-        },
-        {
-          data: [1769, 1562, 1299, 1299, 1339, 1438, 1621, 1885],
-          label: 'Total',
-          borderColor: '#141414',
-          backgroundColor: '#141414',
+          stacked: true,
+          beginAtZero: true,
         },
       ],
     },
-    options: {
-      legend: { position: 'bottom' },
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
-};
-
-trendChart();
+  },
+});
 
 const renderBarChart = () => {
   const ctx = document.getElementById('semChart').getContext('2d');
@@ -78,7 +137,6 @@ const renderBarChart = () => {
   chartLabels.reverse();
   dataIT.reverse();
   dataIS.reverse();
-  const chartConfig = {};
   const barChart = new Chart(ctx, {
     type: 'bar',
     data: {
